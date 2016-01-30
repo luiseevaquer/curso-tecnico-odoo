02336b2cfbb85639a8eedca359a38c38753879a2
Luis Escobar <lescobar@vauxoo.com>
2016-01-30 14:34:05 +0000

    [REF] openacademy: Adding model course

---
+++ openacademy/model/openacademy_course.py
@@ -0,0 +1,14 @@
+from openerp import models, fields, api
+
+'''
+This module create model of Course
+'''
+
+class Course(models.Model):
+    '''
+    This class create model of Course
+    '''
+    _name = 'openacademy.course' # model odoo name
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
