573c3a2759f1a43d4e2f8490f7cbf653dbfd961a
Luis Escobar <lescobar@vauxoo.com>
2016-02-01 04:54:37 +0000

    [REF] openacademy: Adding gantt view

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -24,6 +24,10 @@ class Session(models.Model):
     end_date = fields.Date(string="End Date", store=True,
         compute='_get_end_date', inverse='_set_end_date')
 
+    hours = fields.Float(string="Duration in hours",
+                         compute='_get_hours', inverse='_set_hours')
+
+
     @api.one
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -72,6 +76,16 @@ class Session(models.Model):
         start_date = fields.Datetime.from_string(self.start_date)
         end_date = fields.Datetime.from_string(self.end_date)
         self.duration = (end_date - start_date).days + 1
+
+    @api.one
+    @api.depends('duration')
+    def _get_hours(self):
+        self.hours = self.duration * 24
+
+    @api.one
+    def _set_hours(self):
+            self.duration = self.hours / 24
+
 
     @api.one
     @api.constrains('instructor_id', 'attendee_ids')
