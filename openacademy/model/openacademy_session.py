6d8b88e8fc7ddc9980c0db51c96b619fd7a3d28b
Luis Escobar <lescobar@vauxoo.com>
2016-02-01 05:00:12 +0000

    [REF] openacademy: Adding graph view

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -27,6 +27,8 @@ class Session(models.Model):
     hours = fields.Float(string="Duration in hours",
                          compute='_get_hours', inverse='_set_hours')
 
+    attendees_count = fields.Integer(
+        string="Attendees count", compute='_get_attendees_count', store=True)
 
     @api.one
     @api.depends('seats', 'attendee_ids')
@@ -86,6 +88,10 @@ class Session(models.Model):
     def _set_hours(self):
             self.duration = self.hours / 24
 
+    @api.one
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        self.attendees_count = len(self.attendee_ids)
 
     @api.one
     @api.constrains('instructor_id', 'attendee_ids')
